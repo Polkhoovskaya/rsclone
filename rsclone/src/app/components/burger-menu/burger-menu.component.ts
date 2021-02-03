3053b31fb9403fb7f2898fd9f2578805e06f9c6f
Polkhoovskaya <37327952+Polkhoovskaya@users.noreply.github.com>
2021-02-03 23:38:53 +0300

    Revert "Stylization"

--- rsclone/src/app/components/burger-menu/burger-menu.component.ts
+++ rsclone/src/app/components/burger-menu/burger-menu.component.ts
@@ -1,9 +1,5 @@
-import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
-import {animate, style, transition, trigger} from '@angular/animations';
-import {Subscription} from 'rxjs';
-import {FirebaseService} from '../../services/firebase.service';
-import {AuthenticationService} from '../../services/authentication.service';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-burger-menu',
@@ -28,45 +24,8 @@ import {AuthenticationService} from '../../services/authentication.service';
     ]),
   ],
 })
-export class BurgerMenuComponent implements OnInit, OnDestroy {
-  isShowAuthenticationForm = false;
-  userEmail: string;
-  private subscribtion = new Subscription();
-  isLogged: boolean;
-
+export class BurgerMenuComponent {
   @Output() close: EventEmitter<void> = new EventEmitter<void>();
-
-  constructor(
-    public firebaseService: FirebaseService,
-    public authenticationService: AuthenticationService
-  ) {
-  }
-
-  ngOnDestroy(): void {
-    this.subscribtion.unsubscribe();
-  }
-
-  ngOnInit(): void {
-    this.subscribtion.add(
-      this.authenticationService.isSuccessAuthentication.subscribe(
-        () => {
-          this.isLogged = this.getIsLoggedInfoFromService();
-          this.updateUserEmail();
-        }
-      )
-    );
-  }
-
-  updateUserEmail(): void {
-    this.userEmail = this.authenticationService.userEmail;
-  }
-
-  getIsLoggedInfoFromService(): boolean {
-    return this.authenticationService.isLogged;
-  }
-
-  onOpenAndCloseAuthenticationForm(isOpen: boolean): void {
-    this.isShowAuthenticationForm = isOpen;
 
   toScroll(idElement: string) {
     document.getElementById(idElement).scrollIntoView({ behavior: 'smooth' });
--- rsclone/src/app/components/main-page/start-screen/start-screen.component.ts
+++ rsclone/src/app/components/main-page/start-screen/start-screen.component.ts
@@ -1,49 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {IWasteData} from '../../../models/wasteData.model';
-import {Subscription} from 'rxjs';
-import {FirebaseService} from '../../../services/firebase.service';
-import {AuthenticationService} from '../../../services/authentication.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
   styleUrls: ['./start-screen.component.scss']
 })
-export class StartScreenComponent implements OnInit, OnDestroy {
-  wasteData: IWasteData[];
-  isShowAuthenticationForm = false;
-  userEmail: string;
-  private subscribtion = new Subscription();
-  isLogged: boolean;
-
-  constructor(
-    public firebaseService: FirebaseService,
-    public authenticationService: AuthenticationService
-  ) {}
-
-  ngOnDestroy(): void {
-    this.subscribtion.unsubscribe();
-  }
-
-  ngOnInit(): void {
-    this.subscribtion.add(
-      this.authenticationService.isSuccessAuthentication.subscribe(
-        () => {
-          this.isLogged = this.getIsLoggedInfoFromService();
-          this.updateUserEmail();
-        }
-      )
-    );
-  }
-  updateUserEmail(): void {
-    this.userEmail = this.authenticationService.userEmail;
-  }
-
-  getIsLoggedInfoFromService(): boolean {
-    return this.authenticationService.isLogged;
-  }
-
-  onOpenAndCloseAuthenticationForm(isOpen: boolean): void {
-    this.isShowAuthenticationForm = isOpen;
-  }
+export class StartScreenComponent{
 }
